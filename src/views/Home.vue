7ff9b4799d63e0d5cfbd6e55f26d64df35d1f3e7
DavidBai <baiminghan@gmail.com>
2021-11-22 20:23:22 +1300

    basic settings and understanding of composition API

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,27 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    Home
+    <p>My name is {{ name }} and my age is {{ age }}</p>
+    <button @click="handleClick">Click me</button>
+  </div>
 </template>
 
 <script>
 export default {
   name: "Home",
-  setup() {},
+
+  // the setup hook runs before anything else
+  setup() {
+    // the values are initally not dynamically rendered out
+    let name = "mario";
+    let age = 30;
+
+    const handleClick = () => {
+      console.log("you clicked me");
+    };
+
+    // if the things wanna to be used in the template, have to resigtered via return value
+    return { name, age, handleClick };
+  },
 };
 </script>
