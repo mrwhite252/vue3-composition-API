d660e84d59c2408893f0c4267deb345a9d0693e6
DavidBai <baiminghan@gmail.com>
2021-11-22 20:38:55 +1300

    introduce the refs function

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,35 @@
 <template>
   <div class="home">
     Home
-    <p>My name is {{ name }} and my age is {{ age }}</p>
+    <p ref="para">My name is {{ name }} and my age is {{ age }}</p>
     <button @click="handleClick">Click me</button>
   </div>
 </template>
 
 <script>
+import { ref } from "@vue/reactivity";
 export default {
   name: "Home",
 
   // the setup hook runs before anything else
   setup() {
+    // this keyword is not allowed in the setup hook and composition api
+
+    // introduce ref as the references of the DOM element it hooked with
+    const para = ref(null);
+
     // the values are initally not dynamically rendered out
     let name = "mario";
     let age = 30;
 
     const handleClick = () => {
-      console.log("you clicked me");
+      console.log(para, para.value);
+      para.value.classList.add("test");
+      // para.value.textContent = "hello";
     };
 
     // if the things wanna to be used in the template, have to resigtered via return value
-    return { name, age, handleClick };
+    return { name, age, handleClick, para };
   },
 };
 </script>
