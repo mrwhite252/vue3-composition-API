e9149c7db61d4ff512bfae4394a6c3abb7256c5e
DavidBai <baiminghan@gmail.com>
2021-11-24 20:22:59 +1300

    use async function to fetch data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,40 +1,47 @@
 <template>
   <div class="home">
-    Home
-    <p ref="para">My name is {{ name }} and my age is {{ age }}</p>
-    <button @click="handleClick">Click me</button>
-    <button @click="age++">add age</button>
-    <input type="text" v-model="name" />
+    <h1>Home</h1>
+    <div v-if="error">{{ error }}</div>
+    <div v-if="posts.length">
+      <PostList :posts="posts" />
+    </div>
+    <div v-else>The content is loading...</div>
   </div>
 </template>
 
 <script>
 import { ref } from "@vue/reactivity";
+
+import PostList from "../components/PostList.vue";
+
 export default {
   name: "Home",
+  components: { PostList },
 
   // the setup hook runs before anything else
   setup() {
-    // this keyword is not allowed in the setup hook and composition api
+    const posts = ref([]);
+    const error = ref(null);
 
-    // introduce ref as the references of the DOM element it hooked with
-    const para = ref(null);
+    const load = async () => {
+      try {
+        let data = await fetch("http://localhost:3000/posts");
 
-    // the values are not reactive elements, therefore, ref will solve this porblem and make the element have dynamic values
-    let name = ref("mario");
-    let age = ref(30);
+        posts.value = await data.json();
 
-    const handleClick = () => {
-      name.value = "luigi";
-      age.value = 35;
-
-      // console.log(para, para.value);
-      // para.value.classList.add("test");
-      // para.value.textContent = "hello";
+        if (!data.ok) {
+          throw Error("no data available");
+        }
+      } catch (err) {
+        error.value = err.message;
+        console.log(error.value);
+      }
     };
 
+    load();
+
     // if the things wanna to be used in the template, have to resigtered via return value
-    return { name, age, handleClick, para };
+    return { posts };
   },
 };
 </script>
