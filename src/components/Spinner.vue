8d759eec99ba2abe943efaf341baa08f94062724
DavidBai <baiminghan@gmail.com>
2021-11-24 21:23:44 +1300

    use a new promise function to simulate the delay effect on loading data

---
+++ src/components/Spinner.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="spin"></div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.spin {
+  display: block;
+  width: 40px;
+  height: 40px;
+  margin: 30px auto;
+  border: 3px solid transparent;
+  border-radius: 50%;
+  border-top-color: #ff8800;
+}
+</style>
