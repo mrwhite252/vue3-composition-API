36f577db538f7e5938cbf8a6e9ba4905501a83c9
DavidBai <baiminghan@gmail.com>
2021-11-24 21:15:11 +1300

    add getPost composable and use it in the details.vue

---
+++ src/composables/getPost.js
@@ -0,0 +1,25 @@
+import { ref } from "@vue/reactivity";
+
+const getPost = (id) => {
+  const post = ref(null);
+  const error = ref(null);
+
+  const load = async () => {
+    try {
+      let data = await fetch("http://localhost:3000/posts/" + id);
+
+      post.value = await data.json();
+
+      if (!data.ok) {
+        throw Error("that post does not exist.");
+      }
+    } catch (err) {
+      error.value = err.message;
+      console.log(error.value);
+    }
+  };
+
+  return { post, error, load };
+};
+
+export default getPost;
